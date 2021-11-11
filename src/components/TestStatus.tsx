53f9a248d923170c44e49a7783f34a91f07bd6d3
DmitriBelski <51079926+DmitriBelski@users.noreply.github.com>
2021-11-12 00:41:36 +0300

    Add TestStatus component

---
+++ src/components/TestStatus.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react'
+import { Status } from 'models/Status'
+import capitalize from 'utils/capitalize'
+import classnames from 'utils/classnames'
+import './TestStatus.scss'
+
+interface StatusProps {
+  value: Status;
+}
+
+const TestStatus: React.FC<StatusProps> = (props) => {
+  const statusClass = classnames({
+    'table-medium-font': true,
+    draft: props.value === Status.DRAFT,
+    stopped: props.value === Status.STOPPED,
+    paused: props.value === Status.PAUSED,
+    online: props.value === Status.ONLINE
+  })
+
+  return (
+    <span className={statusClass}>
+      {capitalize(props.value.toLowerCase())}
+    </span>
+  )
+}
+
+export { TestStatus }
